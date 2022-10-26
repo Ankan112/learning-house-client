3bf3f9fabe8c9a1d4c404f4bf60261cb74c9108d
Ankan112 <ankanhalder112@gmail.com>
2022-10-27 05:33:28 +0600

    favicon added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useContext } from 'react';
 import { AuthContext } from '../../Context/UserContext';
 import Toggle from '../../Toggle/Toggle';
+// import { SiSololearn } from 'react-icons/fa';
 
 const Home = () => {
     const { user } = useContext(AuthContext)
@@ -9,7 +10,7 @@ const Home = () => {
         <div>
             <h1>this is home page</h1>
             <p>{user?.email}</p>
-            <p>Name:{user?.displayName}</p>
+            <p>Name:  {user?.displayName}</p>
             <Toggle></Toggle>
         </div>
     );
--- src/pages/Navigation/Navigation.js
+++ src/pages/Navigation/Navigation.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/UserContext';
+import { FaRegUserCircle } from 'react-icons/fa';
 import './Navigation.css'
 
 const Navigation = () => {
@@ -11,7 +12,15 @@ const Navigation = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
             <Container>
-                <Navbar.Brand>Learning House</Navbar.Brand>
+                <Navbar.Brand>
+                    <img
+                        alt=""
+                        src={`https://img.icons8.com/external-flatart-icons-flat-flatarticons/64/000000/external-pencil-high-school-flatart-icons-flat-flatarticons.png`}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{'   '}
+                    Learning House</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
@@ -24,7 +33,15 @@ const Navigation = () => {
                     {
                         user && user.uid ?
 
-                            <Nav>
+                            <Nav className='md:d-flex md:align-items-center'>
+                                <div style={{ cursor: 'pointer' }} title={user.displayName}>
+                                    {
+                                        user.email ?
+                                            <img style={{ width: '30px', height: '30px', borderRadius: '50%' }} src={user.photoURL} alt="" />
+                                            :
+                                            <FaRegUserCircle className='icon' />
+                                    }
+                                </div>
                                 {/* <Link className='route' to="/log">Log Out</Link> */}
                                 <Link className='route' onClick={logOut}>Log Out</Link>
                             </Nav>
