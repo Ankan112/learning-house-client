52b6296f0a7a94c576eef8b0285d53066538aefb
Ankan112 <ankanhalder112@gmail.com>
2022-10-27 22:35:05 +0600

    home page update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,29 +8,29 @@ import './Home.css'
 // import { AuthContext } from '../../Context/UserContext';
 // import { SiSololearn } from 'react-icons/fa';
 
-let sectionStyle = {
-    width: "100%",
-    height: "100vh",
-    backgroundImage: "url(" + { banner } + ")"
-};
+// let sectionStyle = {
+//     width: "100%",
+//     height: "100vh",
+//     backgroundImage: "url(" + { banner } + ")"
+// };
 
 const Home = () => {
     // const { user } = useContext(AuthContext)
     return (
-        <div style={sectionStyle} className=''>
+        <div className=''>
             {/* <h1>this is home page</h1>
             <p>{user?.email}</p>
             <p>Name:  {user?.displayName}</p>
             <Toggle></Toggle> */}
             <Card className="bg-dark text-white text-center">
-                <Card.Img className='w-100' src={banner} alt="Card image" />
-                <Card.ImgOverlay className=' pt-5'>
+                <Card.Img className='w-100 image' src={banner} alt="Card image" />
+                <Card.ImgOverlay className=' d-flex pt-5'>
                     <div>
                         <h1 className='title'>Welcome To Learning House</h1>
                         <p className='w-75 m-auto description'>
                             Online education enables the teacher and the student to set their own learning pace, and there's the added flexibility of setting a schedule that fits everyone's agenda. As a result, using an online educational platform allows for a better balance of work and studies, so there's no need to give anything up.
                         </p>
-                        <Link to='/courses'> <Button className='button' variant="primary">View Courses</Button></Link>
+                        <Link to='/courses'><Button className='button' variant="primary">View Courses</Button></Link>
                     </div>
                 </Card.ImgOverlay>
             </Card>
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import img from '../../img/notfound.png'
 
 const NotFound = () => {
     return (
-        <div className='mt-5'>
-            <h1>404 Not Found</h1>
+        <div className='mt-5 text-center'>
+            <img className='w-50' src={img} alt="" /><br />
             <Link to='/'>Go Home</Link>
         </div>
     );
